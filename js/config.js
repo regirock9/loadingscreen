e282ab19bf712cd6be795b46c120e20d18f0f4c0
regirock9 <omegasword@ymail.com>
2016-01-06 14:15:57 +0800

    2

--- js/config.js
+++ js/config.js
@@ -86,9 +86,6 @@ var l_musicDisplay = true;
 Place how much Youtube IDs/.ogg you want*/
 var l_musicPlaylist = [
 	{ogg: "songs/Lullaby of Deserted Hell.ogg", name: "Lullaby of Deserted Hell"},
-	{youtube: "j0PA0PX7lKo", name: "セプテットはディナーと共に"},
-	{youtube: "yHZfHPhNjXo", name: "いざオペラ座へ"},
-	{youtube: "-7VeTkLKdDw", name: "Departure"}
 ];
 
 /*Random music order?*/
@@ -105,6 +102,7 @@ var l_messagesEnabled = true;
 var l_messages = [
 "Enjoy the server",
 "Thanks for joining us!",
+"Dont Be THEONEANDONLYPOO"
 ];
 
 /*Random message order?*/
